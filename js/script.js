7964c21f7a7758208b738d40dc9b8dda7b434793
Martin <68254461+littlee5521@users.noreply.github.com>
2022-12-06 11:06:59 -0800

    Created function to fill out canvas bug-with higher numbers canvas has dead space

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let counter = 0;
+let counter1 = 0;
+let size = 0;
+const canvas= document.querySelector(".canvas")
+function createGrid(input,){
+    while (counter<input){
+    const wip = document.createElement('div')
+     wip.style.display='flex'
+     wip.style.flex='1'
+     wip.classList.add('row')
+    counter1= 0;
+     while (counter1<input){
+       const pixels = document.createElement('div')
+       pixels.style.flex ='auto'
+       pixels.style.flexShrink = '0'
+       pixels.classList.add('pixel')
+       size = Math.floor(960/input);
+       pixels.style.width = size+'px'
+       pixels.style.height = size+'px'
+       wip.appendChild(pixels)
+       counter1++
+     }
+     canvas.appendChild(wip)
+     counter++;
+    }
+}
+
+createGrid(100)
